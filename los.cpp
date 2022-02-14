580bbe68689d9d921053eb340986f8f24329c8bc
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-13 18:21:35 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,39 +1,32 @@
-int parse(string input) {
+int eval(vector<string> clean) {
+    /*
+    for(string arg : copy)
+      printf("%s\n", arg.c_str());
+    */
 
-    input += " ";
-    vector<string> clean;
-    //looks for words surrounded by spaces and pushes them into clean vector
-    for (unsigned int i = 0; i < input.length(); i++) {
-        if (input.at(i) != ' ') {
-            unsigned int j = i + 1;
-            unsigned int length = 1;
-            if (input.at(i) == '"') {//looking at double quotes
-                while (j < input.length()) {
-                    if (input.at(j) == '"' && input.at(j - 1) != '\\') {
-                        --length;
-                        break;
-                    }
-                    else if (input.at(j) == '"' && input.at(j - 1) == '\\') {
-                        input.erase(j - 1, 1);
-                    }
-                    else {
-                        ++j;
-                        ++length;
-                    }
-                }//while
-                ++i;
-            }//if
-            else {
-                while (input.at(j) != ' ' && j < input.length()) {
-                    ++j;
-                    ++length;
-                }
-            }//else
-            clean.push_back(input.substr(i, length));
-            i = j;
-        }//if
+    if (clean.size() == 0) {
+        return 0;
+    }
 
-    }//for
+    string stdin = "STDIN_FILENO";
+    string stdout = "STDOUT_FILENO";
+    string stderr = "STDERR_FILENO";
 
-    return eval(clean);
-}//parse
+    for (unsigned int i = 0; i < clean.size() - 1; i++) {
+        //printf("%s\n", clean.at(i).c_str());
+        if (clean.at(i).compare("<") == 0) {
+            stdin = clean.at(i + 1);
+        }
+        if (clean.at(i).compare(">") == 0) {
+            stdout = clean.at(i + 1) + " (truncate)";
+        }
+        if (clean.at(i).compare(">>") == 0) {
+            stdout = clean.at(i + 1) + " (append)";
+        }
+        if (clean.at(i).compare("e>") == 0) {
+            stderr = clean.at(i + 1) + " (truncate)";
+        }
+        if (clean.at(i).compare("e>>") == 0) {
+            stderr = clean.at(i + 1) + " (append)";
+        }
+    }//for jobs
