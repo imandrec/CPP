46045b7d8aece62825f25115653845e0eca10b53
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-17 07:48:57 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,63 +1,43 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 
-#define MAXLINE 100
-#define CAT "/bin/cat"   //cat program
-
+//Print the type of file for command-line argument
 int main(int argc, char *argv[]){
-  int n;
-  
-  int  fds[2];
 
-  pid_t  pid;
+    int i;
+    struct stat buf;
 
-  if(pipe(fds) < 0){
-      perror("Pipe error");
-      exit(-1);
-  }
+    char *ptr;
 
-  if((pid = fork()) < 0){
-    perror("Fork error");
-  }else if(pid > 0){
-     //parent
-    close(fds[0]); //close the read end of the pipe
+    for(i = 1; i < argc; i++){
+      printf("%s: ", argv[i]);
+      if(lstat(argv[i], &buf) < 0){
+        perror("lstat error");
+        continue;
+      }
 
-    //re-direct STDOUT_FILENO to fds[1]
-    if(fds[1] != STDOUT_FILENO){
-       if(dup2(fds[1], STDOUT_FILENO) != STDOUT_FILENO) perror("dup2 error to stdout");
-       close(fds[1]); //don't need this afer dup2
+
+      if(S_ISREG(buf.st_mode)) ptr = "regular";
+
+      else if(S_ISDIR(buf.st_mode)) ptr = "directory";
+     
+      else if(S_ISCHR(buf.st_mode)) ptr = "charcter special";
+
+      else if(S_ISBLK(buf.st_mode)) ptr = "block sppecial";
+    
+      else if(S_ISFIFO(buf.st_mode)) ptr = "fifo";
+
+      else if(S_ISLNK(buf.st_mode)) ptr = "symbolic link";
+
+      else if(S_ISSOCK(buf.st_mode)) ptr = "socket";
+
+      else    ptr = "** Unknown Mode **";
+
+      printf("%s\n", ptr);
     }
 
-    //parent process runs /usr/bin/ps -ef
-   if(execl("/bin/ps", "e", (char*)0) < 0){ 
-        perror("PS command failed");
-   }
 
-   if(waitpid(pid, NULL, 0) < 0) perror("Wait error");
+   exit(0);
 
-   exit(0); //terminate parent process
-  }else{
-     //child process
-     close(fds[1]); //close the write end of the pipe
-     if(fds[0] != STDIN_FILENO){
-         if( dup2(fds[0], STDIN_FILENO) != STDIN_FILENO)  perror("dup2 error to stdin");
-
-         close(fds[0]); //don't need this after dup2.
-     }
-
-      char buff[100];
-
-      //int n = read(STDIN_FILENO, buff, 100);
-      
-      write(STDOUT_FILENO, "child", 5);
-     //execute command MORE
-     //if(execl(CAT, argv[0], (char*) 0) < 0)  perror("execl error for cat.");
-  }
-
-  //printf("Parent terminate\n");
-
-  exit(0);
 }
