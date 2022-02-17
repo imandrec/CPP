f4dc3865d211dab75647b8523f3ef0d22fe4dfa8
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-17 07:26:50 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,41 +1,63 @@
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <time.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
-#include <vector>
 
-using namespace std;
+#define MAXLINE 100
+#define CAT "/bin/cat"   //cat program
 
-void getArguments();
-string input="";
-vector<char*> arguments;
+int main(int argc, char *argv[]){
+  int n;
+  
+  int  fds[2];
 
-int main(int argc, char** argv) {
+  pid_t  pid;
 
-cout << "Welcome to my shell. Exit with �logout�.0" << endl;
+  if(pipe(fds) < 0){
+      perror("Pipe error");
+      exit(-1);
+  }
 
-while (input!= "logout") {
-    getline(cin, input);
-    //getArguments();
-    size_t space = input.find(" ");
-    string command= input.substr(0,space);
-    int end = input.length() - space;
-    string text = input.substr(space+1, ende);
+  if((pid = fork()) < 0){
+    perror("Fork error");
+  }else if(pid > 0){
+     //parent
+    close(fds[0]); //close the read end of the pipe
 
-    string path = "/usr/bin/" + command;
-
-    pid_t pid = fork();
-    if(pid == 0)
-    {
-        cout << "Process " << command << " wi: \n";
-        execlp(path.c_str(), NULL);
+    //re-direct STDOUT_FILENO to fds[1]
+    if(fds[1] != STDOUT_FILENO){
+       if(dup2(fds[1], STDOUT_FILENO) != STDOUT_FILENO) perror("dup2 error to stdout");
+       close(fds[1]); //don't need this afer dup2
     }
-    if(text!="&")
-        waitpid(pid, 0, 0);
-}
-return 0;
+
+    //parent process runs /usr/bin/ps -ef
+   if(execl("/bin/ps", "e", (char*)0) < 0){ 
+        perror("PS command failed");
+   }
+
+   if(waitpid(pid, NULL, 0) < 0) perror("Wait error");
+
+   exit(0); //terminate parent process
+  }else{
+     //child process
+     close(fds[1]); //close the write end of the pipe
+     if(fds[0] != STDIN_FILENO){
+         if( dup2(fds[0], STDIN_FILENO) != STDIN_FILENO)  perror("dup2 error to stdin");
+
+         close(fds[0]); //don't need this after dup2.
+     }
+
+      char buff[100];
+
+      //int n = read(STDIN_FILENO, buff, 100);
+      
+      write(STDOUT_FILENO, "child", 5);
+     //execute command MORE
+     //if(execl(CAT, argv[0], (char*) 0) < 0)  perror("execl error for cat.");
+  }
+
+  //printf("Parent terminate\n");
+
+  exit(0);
 }
