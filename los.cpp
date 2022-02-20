639725a454820331ee0bf920de2de17432142c34
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-20 11:31:52 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include <sys/stat.h>
-#include <sys/time.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <string>
 
-#define MAX_SIZE 126
+using namespace std;
 
 //Print the type of file for command-line argument
 int main(int argc, char *argv[]){
-   char buff[100];
+    int i;
+    struct stat buf;
 
-   //get the current working directory of this process
-   if(getcwd(buff, MAX_SIZE) < 0){
-       perror("Failed in getting current working directory");
-   }
-   else{
-      printf("%s\n", buff);
-   }
-   
-   //change the current working directory to /home/gtian
-   if(chdir("/home/cis356") < 0){
-       perror("Failed in changing current working directory");
-   }
-   else{
-        //get the current working directory of this process
-       if(getcwd(buff, MAX_SIZE) < 0){
-           perror("Failed in getting current working directory");
-       }
-       else{
-          printf("%s\n", buff);
-       }
-   }
+    string permission;
+
+    for(i = 1; i < argc; i++){
+      printf("%s: ", argv[i]);
+
+      if(lstat(argv[i], &buf) < 0){
+        perror("lstat error");
+        continue;
+      }
+
+
+      if(S_IRUSR & buf.st_mode) permission += "r";
+      else permission += "-";
+
+      if(S_IWUSR & buf.st_mode) permission += "w";
+      else permission += "-";
+
+      if(S_IXUSR & buf.st_mode) permission += "x";
+      else permission += "-";
+
+      printf("%s\n", permission.c_str());
+    }
+
 
    exit(0);
-
-}
