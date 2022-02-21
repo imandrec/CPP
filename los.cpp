6e43b224a94e95dbdad3693fb67b2d723e685426
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-21 16:40:24 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,12 +1,27 @@
-{
-  "version": "0.2.0",
-  "configurations": [
-    {
-      "type": "node",
-      "request": "launch",
-      "name": "Launch Program",
-      "skipFiles": ["<node_internals>/**"],
-      "program": "${workspaceFolder}\\app.js"
-    }
-  ]
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+//Print the type of file for command-line argument
+int main(int argc, char *argv[]){
+    int fd;
+
+    mode_t oldmask;
+
+    oldmask = umask(S_IRWXG); //set the Group's RWX bit on
+
+    printf("Old umask is %i\n", oldmask);
+
+    if((fd=creat("temp.txt", S_IRWXU | S_IRWXG)) < 0) perror("creat() error");
+    else{
+       system("ls -l temp.txt");
+       close(fd);
+     }
+    
+   umask(oldmask);
+
+   exit(0);
+
 }
