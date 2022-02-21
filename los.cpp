5627de3db778f9ab862bcc548718c668fba9983d
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-21 16:14:23 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -1,37 +1,12 @@
-#include <stdio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string>
-
-using namespace std;
-
-//Print the type of file for command-line argument
-int main(int argc, char *argv[]){
-    int i;
-    struct stat buf;
-
-    string permission;
-
-    for(i = 1; i < argc; i++){
-      printf("%s: ", argv[i]);
-
-      if(lstat(argv[i], &buf) < 0){
-        perror("lstat error");
-        continue;
-      }
-
-
-      if(S_IRUSR & buf.st_mode) permission += "r";
-      else permission += "-";
-
-      if(S_IWUSR & buf.st_mode) permission += "w";
-      else permission += "-";
-
-      if(S_IXUSR & buf.st_mode) permission += "x";
-      else permission += "-";
-
-      printf("%s\n", permission.c_str());
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "type": "node",
+      "request": "launch",
+      "name": "Launch Program",
+      "skipFiles": ["<node_internals>/**"],
+      "program": "${workspaceFolder}\\app.js"
     }
-
-
-   exit(0);
+  ]
+}
