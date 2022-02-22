963b9664fa3f174c185da5ae11ce07bbc6ce9f78
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-02-21 19:02:16 -0600

    Update los.cpp

--- los.cpp
+++ los.cpp
@@ -6,22 +6,35 @@
 
 //Print the type of file for command-line argument
 int main(int argc, char *argv[]){
+    struct stat buf;
+
+    //create a file temp
     int fd;
 
-    mode_t oldmask;
-
-    oldmask = umask(S_IRWXG); //set the Group's RWX bit on
-
-    printf("Old umask is %i\n", oldmask);
-
-    if((fd=creat("temp.txt", S_IRWXU | S_IRWXG)) < 0) perror("creat() error");
+    if((fd=creat("temp", 0444)) < 0) perror("creat() error");
     else{
-       system("ls -l temp.txt");
+       system("ls -l temp"); //print temp file's permission
+
+       //get current permission of temp file
+       if(stat("temp", &buf) < 0){
+          perror("Stat error for temp");
+       }
+        //turn on set-group-ID and turn off group-execute
+       if(chmod("temp", buf.st_mode & ~S_IXGRP | S_ISGID) <0 ){
+           perror("chmod failed for temp");
+       }
+
+       system("ls -l temp");
+
+       //set absolute mode to "rw-r--r--"
+       if(chmod("temp", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) < 0){
+          perror("chmod error for temp");
+       }
+
        close(fd);
      }
-    
-   umask(oldmask);
 
+   system("ls -l temp");
    exit(0);
 
 }
