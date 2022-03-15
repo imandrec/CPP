96cd05e989591558ac2224fb5d6b2ef9f56e1571
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-03-15 07:39:26 -0500

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -1,45 +1,48 @@
+//use sigpromask to block all signlas except USER1
 #include <stdio.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(){
-    sigset_t set, oset;
+  sigset_t set, oset;
 
-    //set the signal mask to block nothing
-    if(sigemptyset(&set) < 0){
-        perror("Sigemptyset fail");
-        exit(-1);
-    }
-
-    //add SIGINT
-    if(sigaddset(&set, SIGINT) < 0){
-      perror("Add SIGINT failed");
-      exit(-1);
-    }
-
-    //check if SIGINT exists in the set
-    if(sigismember(&set, SIGINT) == 1){
-      printf("SIGINT is set");
-    }else{
-        printf("SIGINT is not set");
-    }
-
-
-   fflush(stdout); //flush the standard output immediately
-
-    //set the signalpromask to block SIGINT so Ctrl + C cannot stop the process
-    if(sigprocmask(SIG_BLOCK, &set, &oset) == -1){
-      perror("Sigpromask() failed");
-      exit(-1);
-    }
-
-
-    //let the program sleep for 30 seconds and then terminate
-    sleep(30); 
-
-    printf("Terminated");
+  //set the singal mask to block everything
+  if(sigfillset(&set) == -1){
+    perror("Sigfillset failed");
     exit(-1);
+  }
 
+  if(sigdelset(&set, SIGUSR1) == -1){
+    perror("sigdelset USR1 failed");
+    exit(-1);
+  }
+
+ //Beginning of signal free region
+ if(sigprocmask(SIG_BLOCK, &set, &oset) == -1){
+   perror("sigprocmask failed");
+   exit(-1);
+  } 
+
+  //sleep for 1 minute.  Critical region - blocks signal interruptions
+  for(int i = 0; i < 6; i++){
+    sleep(10); //sleep for 10 seconds.
+    //write(1, "Sleep\n", 7);
+    printf("Sleep\n");
+  }//critical region (signal free)
+
+  //unblock the signals
+  if( sigprocmask(SIG_SETMASK, &oset, NULL) == -1){
+     perror("reset old mask failed");
+     exit(-1);
+  }
+
+  while(1){
+    sleep(10);
+    //printf("try to do someting forever!");
+    write(1, "Sleep forever", 14);
+  }
+
+
+  exit(0);
 }
