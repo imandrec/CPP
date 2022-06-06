5485b17dc400eb038e54ca7853fa6707d53c6855
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-06-06 13:05:21 -0500

    Update doublelinklist.cpp

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -2,127 +2,148 @@
 
 using namespace std;
 
-struct Node{
+struct Node
+{
    int data;
-   Node * prev;
-   Node * next;
+   Node *prev;
+   Node *next;
 };
 
-struct MyList{
-   Node * head;
-   //Node * tail;
+struct MyList
+{
+   Node *head;
+   // Node * tail;
 };
 
-bool IsEmpty(MyList list){
-   if(list.head == nullptr) return true;
-   else return false;
+bool IsEmpty(MyList list)
+{
+   if (list.head == nullptr)
+      return true;
+   else
+      return false;
 }
 
-void Insert(MyList & list, int data){
-   Node * node = new Node();
+void Insert(MyList &list, int data)
+{
+   Node *node = new Node();
 
    node->data = data;
    node->prev = nullptr;
-   node->next = list.head;   //node->next points to NULL
+   node->next = list.head; // node->next points to NULL
 
-   if(list.head == nullptr){
+   if (list.head == nullptr)
+   {
       list.head = node;
       node->prev = nullptr;
-   }else{
-     //insert the new node at the beginning of the list
-     list.head->prev = node;
-     list.head = node;
-     node->prev = nullptr;
+   }
+   else
+   {
+      // insert the new node at the beginning of the list
+      list.head->prev = node;
+      list.head = node;
+      node->prev = nullptr;
    }
 }
 
-//Traverse the list from the head
-void PrintAll(const MyList & list){
-  Node * temp = list.head;
+// Traverse the list from the head
+void PrintAll(const MyList &list)
+{
+   Node *temp = list.head;
 
-  if(temp == nullptr){
-    std::cout << "list is empty" << std::endl;
-  }else{
-    while(temp != nullptr)
-    {
-       std::cout << temp->data << std::endl;
-       temp = temp->next;
-    }
+   if (temp == nullptr)
+   {
+      std::cout << "list is empty" << std::endl;
+   }
+   else
+   {
+      while (temp != nullptr)
+      {
+         std::cout << temp->data << std::endl;
+         temp = temp->next;
+      }
 
-     std::cout << "*************************************" << std::endl;
-  }
+      std::cout << "*************************************" << std::endl;
+   }
 }
 
-Node * Search(const MyList & list, int key){
-    Node * temp = list.head;
+Node *Search(const MyList &list, int key)
+{
+   Node *temp = list.head;
 
-    while(temp != nullptr && temp->data != key)
-    {
-        temp = temp->next;
-    }
+   while (temp != nullptr && temp->data != key)
+   {
+      temp = temp->next;
+   }
 
-    return temp;
+   return temp;
 }
 
-void Delete(MyList & list, int key){
-     Node * temp = Search(list, key); //call search() 
+void Delete(MyList & list, int key)
+{
+   Node *temp = Search(list, key); // call search()
 
-     if(temp != nullptr)
-     {
-         if(temp->prev != nullptr)
-         {
-             temp->prev->next = temp->next;
-         }
-         else
-         {
-             list.head = temp->next;
-         }
+   if (temp != nullptr)
+   {
+      if (temp->prev != nullptr)
+      {
+         temp->prev->next = temp->next;
+      }
+      else
+      {
+         list.head = temp->next;
+      }
 
-         if(temp->next != nullptr)
-         {
-             temp->next->prev = temp->prev;
-         }
-    }
+      if (temp->next != nullptr)
+      {
+         temp->next->prev = temp->prev;
+      }
+   }
 }
 
-int main(){
+int main()
+{
 
-  MyList list;
+   MyList list;
 
-  list.head = nullptr;    //initialize the linked-list
+   list.head = nullptr; // initialize the linked-list
 
-  if(IsEmpty(list))  std::cout << "List is empty" << std::endl;
+   if (IsEmpty(list))
+      std::cout << "List is empty" << std::endl;
 
-  Insert(list, 10);
+   Insert(list, 10);
 
-  Insert(list, 20);
+   Insert(list, 20);
 
-  Insert(list, 30);
+   Insert(list, 30);
 
-  Insert(list, 40);
+   Insert(list, 40);
 
-  PrintAll(list);
+   Insert(list, 50);
 
-  int x = 24;
-  Node * result = Search(list, x);
+   PrintAll(list);
 
-  if(result == nullptr) cout << "Cannot find " << x << endl;
-  else cout << "Found " << result->data << endl;
+   int x = 24;
+   Node *result = Search(list, x);
 
-  Delete(list, 200);
-  PrintAll(list);
+   if (result == nullptr)
+      cout << "Cannot find " << x << endl;
+   else
+      cout << "Found " << result->data << endl;
 
-  Delete(list,10);
-  PrintAll(list);
+   Delete(list, 200);
+   PrintAll(list);
 
-  Delete(list,40);
-  PrintAll(list);
+   Delete(list, 10);
+   PrintAll(list);
 
-  Delete(list,20);
-  PrintAll(list);
+   Delete(list, 40);
+   PrintAll(list);
 
-  Delete(list,30);
-  PrintAll(list);
+   Delete(list, 20);
+   PrintAll(list);
 
-  return 0;
+   Delete(list, 30);
+   PrintAll(list);
+
+   return 0;
 }
