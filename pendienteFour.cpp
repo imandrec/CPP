b0d978b9c3177b4cdf815478f61f8e2c5d885a89
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-06-21 10:36:06 -0500

    Update pendienteFour.cpp

--- pendienteFour.cpp
+++ pendienteFour.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 using namespace std;
 
-// A utility function that returns
-// maximum of two integers
+// Check what is the maximum of 2 int
 int max(int a, int b) { return (a > b) ? a : b; }
 
-// Returns the maximum value that
-// can be put in a knapsack of capacity W
+// Returns the maximum value we can put in a knapsack
 int knapSack(int Capacity, int Weight[], int Value[], int n)
 {
 
-	// Base Case
 	if (n == 0 || Capacity == 0)
 		return 0;
 
-	// If weight of the nth item is more
-	// than Knapsack capacity W, then
-	// this item cannot be included
-	// in the optimal solution
+	// If weight of an item is greater than capacity we cant include the item
 	if (Weight[n - 1] > Capacity)
 		return knapSack(Capacity, Weight, Value, n - 1);
 
@@ -26,19 +20,18 @@ int knapSack(int Capacity, int Weight[], int Value[], int n)
 	// (2) not included
 	else
 		return max(
-			Value[n - 1]
-			+ knapSack(Capacity - Weight[n - 1],
-				Weight, Value, n - 1),
+			Value[n - 1] + knapSack(Capacity - Weight[n - 1],
+			Weight, Value, n - 1),
 			knapSack(Capacity, Weight, Value, n - 1));
 }
 
-// Driver code
+//Main function
 int main()
 {
 	int Value[] = { 60, 100, 120 };
 	int Weight[] = { 10, 20, 30 };
 	int Capacity = 50;
 	int n = sizeof(Value) / sizeof(Value[0]);
-	cout << knapSack(Capacity, Weight, Value, n);
+	cout << "The optime solution is: " << knapSack(Capacity, Weight, Value, n);
 	return 0;
 }
